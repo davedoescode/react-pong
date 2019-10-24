fd5f3d664448e76b9c1c5704bd83462522f1f145
davedoescode <mddavies@ualberta.ca>
2019-10-23 20:34:32 -0600

    added board class to make unit tests pass

--- src/classes/Board/index.js
+++ src/classes/Board/index.js
@@ -1,3 +1,27 @@
-class Board {}
+class Board {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  drawBackground(ctx) {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, this.width, this.height);
+  }
+
+  drawDivider(ctx) {
+    ctx.strokeStyle = 'white';
+
+    ctx.beginPath();
+    ctx.moveTo(this.width / 2, 0);
+    ctx.lineTo(this.width / 2, this.height);
+    ctx.stroke();
+  }
+
+  draw(ctx) {
+    this.drawBackground(ctx);
+    this.drawDivider(ctx);
+  }
+}
 
 export default Board;
