4c3484af43ed79d63c2704fb9f4b5bf6d136468f
davedoescode <mddavies@ualberta.ca>
2019-10-23 20:52:11 -0600

    add Scoreboard class to pass tests

--- src/classes/Scoreboard/index.js
+++ src/classes/Scoreboard/index.js
@@ -1,3 +1,44 @@
-class Scoreboard {}
+class Scoreboard {
+  constructor(position, size, scores) {
+    this.position = position;
+    this.size = size;
+    this.scores = scores;
+  }
+
+  setScore(scoreIndex, score) {
+    this.scores[scoreIndex] = score;
+  }
+
+  incrementScore(scoreIndex) {
+    this.scores[scoreIndex] += 1;
+  }
+
+  draw(ctx) {
+    const width = this.size;
+    const height = this.size;
+    const { x, y } = this.position;
+
+    // draw box
+    ctx.strokeStyle = 'white';
+    ctx.strokeRect(x - width, y, width, height);
+    ctx.strokeRect(x, y, width, height);
+
+    // draw scores
+    ctx.fillStyle = 'white';
+    ctx.font = `${this.size / 2}px Arial`;
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.fillText(
+      this.scores[0],
+      x - (width / 2),
+      y + (height / 2),
+    );
+    ctx.fillText(
+      this.scores[1],
+      x + (width / 2),
+      y + (height / 2),
+    );
+  }
+}
 
 export default Scoreboard;
