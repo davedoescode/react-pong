3661864cf69a673b2407e46e493a00b12ce9bb4b
davedoescode <mddavies@ualberta.ca>
2019-10-23 20:30:38 -0600

    add Board folder in classes and make unit tests

---
+++ src/classes/Board/index.test.js
@@ -0,0 +1,76 @@
+import Board from './index';
+
+describe('Board', () => {
+  let width;
+  let height;
+
+  beforeEach(() => {
+    width = 200;
+    height = 100;
+  });
+
+  const createBoardInstance = () => new Board(width, height);
+
+  describe('draw', () => {
+    let ctx;
+
+    beforeEach(() => {
+      ctx = {
+        fillRect: jest.fn(),
+        fillStyle: '',
+        strokeStyle: '',
+        beginPath: jest.fn(),
+        moveTo: jest.fn(),
+        lineTo: jest.fn(),
+        stroke: jest.fn(),
+      };
+    });
+
+    it('makes a rectangle with dimensions (width, height)', () => {
+      createBoardInstance().draw(ctx);
+      expect(ctx.fillRect.mock.calls[0])
+        .toEqual([0, 0, width, height]);
+    });
+
+    it('makes the rectangle black', () => {
+      createBoardInstance().draw(ctx);
+      expect(ctx.fillStyle).toBe('black');
+    });
+
+
+    describe('drawDivider', () => {
+      let startingPoint;
+      let endingPoint;
+
+      beforeEach(() => {
+        startingPoint = [width / 2, 0];
+        endingPoint = [width / 2, height];
+      });
+
+      it('sets line color to white', () => {
+        createBoardInstance().draw(ctx);
+        expect(ctx.strokeStyle).toBe('white');
+      });
+
+      it('starts drawing', () => {
+        createBoardInstance().draw(ctx);
+        expect(ctx.beginPath).toHaveBeenCalled();
+      });
+
+      it('moves to starting point of line', () => {
+        createBoardInstance().draw(ctx);
+        expect(ctx.moveTo.mock.calls[0]).toEqual(startingPoint);
+      });
+
+      it('makes a line to the ending point', () => {
+        createBoardInstance().draw(ctx);
+        expect(ctx.lineTo.mock.calls[0]).toEqual(endingPoint);
+      });
+
+      it('sets the line to have stroke', () => {
+        createBoardInstance().draw(ctx);
+        expect(ctx.stroke).toHaveBeenCalled();
+      });
+    });
+  });
+});
